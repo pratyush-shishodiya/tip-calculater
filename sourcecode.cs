a5576601f795101577af5b757b03bbec7db52757
pratyush-shishodiya <76863772+pratyush-shishodiya@users.noreply.github.com>
2022-01-10 23:35:56 +0530

    Update sourcecode.cs

--- sourcecode.cs
+++ sourcecode.cs
@@ -1,27 +1,54 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace tipcalculater
+namespace TipCalculator
 {
-    class program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to tip calculater.\nPlease enter the amount");
-            double amount=double.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter the tip percentage you want to give.");
-            double tippercentage=double.Parse(Console.ReadLine());
-            double tipamount=0,totalpayble=0;
-            if(tippercentage>20.0)
-            {
-                Console.WriteLine("Tip is above limit.");
 
-            }
-            if(tippercentage<0.0){
-                tipamount=0;
-            }
-            tipamount=tippercentage/100*amount;
-            totalpayble=amount+tipamount;
-            Console.WriteLine($"Total amount to pay:{totalpayble:c}\nTotal Purachse:{amount:c}\nTip amount:{tipamount:c}");
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        string answer;
+        float totalPrice;
+        float tippercentage;
+        int people;
+        float tipperhead;
+        float billperhead;
+        Console.WriteLine("Would you like to calculate a tip?");
+        answer = Console.ReadLine();
+        if (answer == "yes")
+        {
+            Console.WriteLine("Please enter the total price of your meal");
+            totalPrice = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Please enter the tip percantage");
+            tippercentage = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Please enter the number of people");
+            people = int.Parse(Console.ReadLine());
+            
+            tipperhead=totalPrice*(tippercentage/100);
+            tipperhead=tipperhead/2;
+        
+            
+            billperhead=totalPrice/2+tipperhead;
+            
+            Console.WriteLine($"Total amount to pay:{totalPrice:c}\nbill per person:{billperhead:c}\nTip amount per person:{tipperhead:c}");
+
+            
+
         }
+        else if (answer == "no")
+            Console.WriteLine("Please run this again when you wish to calculate a tip.");
+        else
+            Console.WriteLine("Error. Please type in \"yes\" or \"no\"");
+
+        Console.ReadLine();
     }
+
+}
 }
